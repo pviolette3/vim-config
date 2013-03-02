6347f8c99e650141a62455eed77fd57a450469e0
Patrick Violette <patrick@ubuntu.(none)>
2013-03-01 19:31:33 -0800

    worked more on the vim installation script

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
-ln -s ~/.vim/vimrc ~/.vimrc
+#!/bin/sh
+rm -rf ~/.vim
+mkdir ~/.vim
+cp -R config ~/.vim/config
+cp vimrc ~/.vim/vimrc
+cp vundle.vim ~/.vim/vundle.vim
 rm -rf ~/.vim/bundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-echo "You should be good to go...Just run :BundleInstall from a vim shell."
+vim -c :BundleInstall -c :q -c :q
+echo -n "Vim has finished configuring plugins. Start it now? (y/n):"
+read response
+echo "$response"
+if [ "$response" == "y" ]; then
+  echo "Starting vim now....."
+  vim
+else
+  echo "Starting vim later. Bye."
+fi
